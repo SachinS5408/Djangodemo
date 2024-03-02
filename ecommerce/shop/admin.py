3ef93208849d43c19b9fec2facd9053ba17386f5
SachinS5408 <kailassachin5408@gmail.com>
2024-03-02 14:28:03 +0530

    added2

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from shop.models import category,Product
+from django.http import HttpResponse
 # Register your models here.
 admin.site.register(category)
 admin.site.register(Product)
